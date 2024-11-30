301afe019e9654f124eb0b95a8a3c992b6bd5e07
Vladimir <vi142696@gmail.com>
2024-11-30 01:06:10 +0100

    feat: schema validation

--- src/storage/table/tuple.h
+++ src/storage/table/tuple.h
@@ -17,6 +17,30 @@ namespace storage {
             if (fields_.size() != schema_.GetColumnCount()) {
                 throw std::invalid_argument("Number of fields doesn't match schema");
             }
+            for (size_t i = 0; i < fields_.size(); ++i) {
+                const Column& column = schema_.GetColumn(i);
+                const Field& field = fields_[i];
+
+                switch (column.type) {
+                    case INTEGER:
+                        if (!std::holds_alternative<int>(field)) {
+                            throw std::invalid_argument("Field type mismatch: expected INTEGER");
+                        }
+                        break;
+                    case DOUBLE:
+                        if (!std::holds_alternative<double>(field)) {
+                            throw std::invalid_argument("Field type mismatch: expected DOUBLE");
+                        }
+                        break;
+                    case VARCHAR:
+                        if (!std::holds_alternative<std::string>(field)) {
+                            throw std::invalid_argument("Field type mismatch: expected VARCHAR");
+                        }
+                        break;
+                    default:
+                        throw std::invalid_argument("Unknown column type in schema");
+                }
+            }
         }
 
         const Field& GetField(size_t index) {
